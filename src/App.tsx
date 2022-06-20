9f90b7bfd63200699d46fab97240c4fde8c0f77e
Tom Smith <thomas-smith@sky.com>
2022-06-20 20:51:09 +0100

    Moved to react + typescript. Redesigned website

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style/App.css';
+import TomSmith from './components/TomSmith';
+ 
+function App() {
+  return (
+    <TomSmith />
+  );
+}
+
+export default App;
